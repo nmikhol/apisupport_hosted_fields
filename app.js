0557ab37b5a7c27be39e21466ecfdf0a5b997403
nmikhol <nataliemikhol@gmail.com>
2020-01-12 14:34:46 -0600

    create customer and transaction from token

--- app.js
+++ app.js
@@ -24,5 +24,56 @@ app.listen(PORT, () => console.log(`App is up and running listening on port ${PO
 app.get('/', (req, res) => {
   gateway.clientToken.generate({},(err, response) => {
     res.render('checkout', {clientToken: response.clientToken})
+    //console.log(response.clientToken)
     })
   })
+
+  app.post('/transaction', (req, res, next) => {
+    let paymentNonce = req.body.payment_method_nonce
+    let firstName = req.body.firstName
+    let lastName = req.body.lastName
+    let postalCode = req.body.postalCode
+    let amount = req.body.amount
+    let email = req.body.email
+
+
+    let newCustomer = gateway.customer.create({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      paymentMethodNonce:paymentNonce,
+      creditCard: {
+        billingAddress: {
+          postalCode: postalCode
+        },
+        options: {
+          verifyCard: true
+        }
+      }
+    }, function (error, result) {
+        if (result.success) {
+          console.log(result.customer.id)
+          gateway.transaction.sale({
+            amount: amount,
+            paymentMethodToken: result.customer.creditCards[0].token,
+            options: {
+              submitForSettlement: true
+              }
+            }, function(error, result) {
+                if (result.success || result.transaction) {
+                  res.render('results', {transactionResponse: result, customerResponse: result});
+                  } else {
+                      transactionErrors = result.errors.deepErrors();
+                      console.log(transactionErrors)
+                      //req.flash('error', {msg: formatErrors(transactionErrors)});
+                      res.render('results');
+                    }
+                  });
+        } else {
+          customerErrors = result.errors.deepErrors();
+          console.log(customerErrors)
+          //req.flash('error', {msg: formatErrors(customerErrors)});
+          res.render('checkout');
+        }
+        });
+    });
